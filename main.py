d9b282b600edfc8ff7d7c52fe645c75bc86dc630
Steven Kraft <steven.kraftjp@gmail.com>
2018-04-21 15:15:09 -0400

    Added Set and Get Clipboard Methods and Regex Clipboard Modification

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
 from tkinter import *
+import win32clipboard
+import re
+
+def getClipboard():
+    win32clipboard.OpenClipboard()
+    data = win32clipboard.GetClipboardData()
+    win32clipboard.CloseClipboard()
+    return data
+
+def setClipboard(data):
+    win32clipboard.OpenClipboard()
+    win32clipboard.EmptyClipboard()
+    win32clipboard.SetClipboardText(data)
+    win32clipboard.CloseClipboard()
+
+def performReg(reg):
+    reg = re.compile(reg)
+    text = getClipboard()
+    result = reg.findall(text)[0]
+    #TODO Add Notification that Modification was made
+    setClipboard(result)
+    return result
 
 root = Tk()
 
-w = Label(root, text="Hello, world!")
+w = Label(root, text="Hello!")
 w.pack()
 
 root.mainloop()
